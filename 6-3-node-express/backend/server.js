ebf13622753cab321b543a364de6dbc0d250b790
omaralshehri <Alshehri2292@gmail.com>
2026-04-06 08:32:18 +0300

    Complete task 6.1 and 6.2

--- 6-3-node-express/backend/server.js
+++ 6-3-node-express/backend/server.js
@@ -14,9 +14,15 @@ app.use(morgan("dev"));
 import { getRandomQuote} from "./quotes.js";
 
 // TODO 6.1: Create root route "/" 
+app.get("/",(req,res)=>{
+    res.send("Hello from Express server");
+});
 
 
 // TODO 6.2: Create "/api/quote" route
-
-
+app.get("/api/quote",(req,res)=>{
+    const quote=getRandomQuote();
+    res.json({quote});
+    });
+    
 // TODO 7: Start server using app.listen
