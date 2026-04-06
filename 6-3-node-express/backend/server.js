3d57559911056d5f67595eac81bcb506a80c1eba
omaralshehri <Alshehri2292@gmail.com>
2026-04-06 08:25:52 +0300

    Complete the quote import and the funcation in quotes.js

--- 6-3-node-express/backend/server.js
+++ 6-3-node-express/backend/server.js
@@ -5,10 +5,13 @@ const app =express();
 const PROT=3000;
 
 // TODO 4: Import and Apply CORS middleware
-
+import cors from "cors";
+app.use(cors());
 
 // TODO 5: Import and apply Morgan middleware
-
+import morgan from "morgan";
+app.use(morgan("dev"));
+import { getRandomQuote} from "./quotes.js";
 
 // TODO 6.1: Create root route "/" 
 
